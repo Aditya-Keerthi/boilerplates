552b493e0463e3c4fc83cf5cc6e78252db5c2b30
adityakeerthi <54603000+Aditya-Keerthi@users.noreply.github.com>
2020-08-23 16:21:37 -0400

    Adding React Application Boilerplate

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { Input } from 'antd';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Application
+      </div> 
+    )
+  }
+
+}
